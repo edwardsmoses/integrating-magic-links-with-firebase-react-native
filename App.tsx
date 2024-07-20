22b10b498b451a22bc7c45d7748e4816fce893c0
Edwards Moses <edwardsmoses3@gmail.com>
2024-07-20 15:36:25 +0100

    feat: complete magic link signup

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -16,14 +16,39 @@ import {
   Alert,
   TextInput,
   Button,
+  ActivityIndicator,
 } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
 
-import auth from '@react-native-firebase/auth';
-
-const MagicLinkSignIn = () => {
+const MagicLinkLogin = () => {
   const [email, setEmail] = useState('');
+
+  const BUNDLE_ID =
+    'org.reactjs.native.example.reactnative-firebase-magiclink-app';
+
+  const sendSignInLink = async (email: string) => {
+    try {
+      await AsyncStorage.setItem('emailForSignIn', email);
+      await auth().sendSignInLinkToEmail(email, {
+        handleCodeInApp: true,
+        url: 'https://bike-rentals-5f360.web.app',
+        iOS: {
+          bundleId: BUNDLE_ID,
+        },
+        android: {
+          packageName: BUNDLE_ID,
+        },
+      });
+
+      Alert.alert(`Login link sent to ${email}`);
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error sending login link');
+    }
+  };
 
   return (
     <View style={styles.authContainer}>
@@ -42,28 +67,82 @@ const MagicLinkSignIn = () => {
   );
 };
 
-const BUNDLE_ID =
-  'org.reactjs.native.example.reactnative-firebase-magiclink-app';
+const useEmailLinkEffect = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-const sendSignInLink = async (email: string) => {
-  try {
-    await AsyncStorage.setItem('emailForSignIn', email);
-    await auth().sendSignInLinkToEmail(email, {
-      handleCodeInApp: true,
-      url: 'https://bike-rentals-5f360.web.app',
-      iOS: {
-        bundleId: BUNDLE_ID,
-      },
-      android: {
-        packageName: BUNDLE_ID,
-      },
-    });
+  useEffect(() => {
+    const handleDynamicLink = async (link: any) => {
+      console.log('what is link', link);
+      if (auth().isSignInWithEmailLink(link.url)) {
+        setLoading(true);
 
-    Alert.alert(`Login link sent to ${email}`);
-  } catch (error) {
-    console.error(error);
-    Alert.alert('Error sending login link');
+        try {
+          const email = await AsyncStorage.getItem('emailForSignIn');
+          await auth().signInWithEmailLink(email as string, link.url);
+        } catch (e: any) {
+          setError(e);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
+    dynamicLinks()
+      .getInitialLink()
+      .then(link => link && handleDynamicLink(link));
+
+    return () => unsubscribe();
+  }, []);
+
+  return {error, loading};
+};
+
+const EmailLinkHandler = () => {
+  const {loading, error} = useEmailLinkEffect();
+
+  if (loading || error) {
+    return (
+      <View style={[StyleSheet.absoluteFillObject, styles.loadingContainer]}>
+        {loading && <ActivityIndicator />}
+      </View>
+    );
   }
+
+  return null;
+};
+
+const MagicLinkSignIn = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  // Handle user state changes
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(authUser => {
+      setUser(authUser);
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+    return () => subscriber(); // unsubscribe on unmount
+  }, [initializing]);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return <MagicLinkLogin />;
+  }
+
+  return (
+    <View style={styles.authContainer}>
+      <Text style={styles.sectionTitle}>Welcome {user.email}</Text>
+    </View>
+  );
 };
 
 function App(): React.JSX.Element {
@@ -80,6 +159,7 @@ function App(): React.JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <View style={styles.sectionContainer}>
+        <EmailLinkHandler />
         <MagicLinkSignIn />
       </View>
     </SafeAreaView>
@@ -94,6 +174,11 @@ const styles = StyleSheet.create({
   authContainer: {
     flex: 1,
     justifyContent: 'center',
+  },
+  loadingContainer: {
+    backgroundColor: 'rgba(250,250,250,0.33)',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textInput: {
     borderColor: 'blue',

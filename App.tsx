e21d2d90e6df1831e9392001bf4e89f66a93e580
Edwards Moses <edwardsmoses3@gmail.com>
2024-07-20 15:12:46 +0100

    feat: complete configuration of auth with firebase

--- App.tsx
+++ App.tsx
@@ -5,61 +5,72 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Alert,
+  TextInput,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import auth from '@react-native-firebase/auth';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const MagicLinkSignIn = () => {
+  const [email, setEmail] = useState('');
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.authContainer}>
+      <Text style={styles.sectionTitle}>Magic Link Sign In</Text>
+      <TextInput
+        style={styles.textInput}
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <Button
+        color={'007AFF'}
+        title="Send login link"
+        onPress={() => sendSignInLink(email)}
+      />
     </View>
   );
-}
+};
+
+const BUNDLE_ID =
+  'org.reactjs.native.example.reactnative-firebase-magiclink-app';
+
+const sendSignInLink = async (email: string) => {
+  try {
+    await AsyncStorage.setItem('emailForSignIn', email);
+    await auth().sendSignInLinkToEmail(email, {
+      handleCodeInApp: true,
+      url: 'https://bike-rentals-5f360.web.app',
+      iOS: {
+        bundleId: BUNDLE_ID,
+      },
+      android: {
+        packageName: BUNDLE_ID,
+      },
+    });
+
+    Alert.alert(`Login link sent to ${email}`);
+  } catch (error) {
+    console.error(error);
+    Alert.alert('Error sending login link');
+  }
+};
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: 'lightgray',
   };
 
   return (
@@ -68,38 +79,30 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={styles.sectionContainer}>
+        <MagicLinkSignIn />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
     paddingHorizontal: 24,
+    height: '100%',
+  },
+  authContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textInput: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginTop: 16,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 16,
   },
   sectionTitle: {
     fontSize: 24,
